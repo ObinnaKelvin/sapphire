a6eff8b1f8b4e1513656baac8615d3a6e0e99281
Obinna Okere <okereobinna10@gmail.com>
2023-10-03 15:11:14 +0100

    Gallery Page Added both for mobile and desktop view

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './View/pages/home/Home';
 import AboutUs from './View/pages/about/AboutUs';
 import Blog from './View/pages/blog/Blog';
-// import Gallery from './View/pages/gallery/Gallery'
+import Gallery from './View/pages/gallery/Gallery'
 // import Contact from './View/pages/contact/Contact'
 // import BookPage from './View/pages/bookPage/BookPage'
 import './App.scss'
@@ -14,8 +14,8 @@ function App() {
           <Route path='/' element={<Home />}/>
           <Route path='/about-us' element={<AboutUs />}/>
           <Route path='/blog' element={<Blog />}/>
-          {/* <Route path='/gallery' element={<Gallery />}/>
-          <Route path='/contact-us' element={<Contact />}/>
+          <Route path='/gallery' element={<Gallery />}/>
+          {/* <Route path='/contact-us' element={<Contact />}/>
           <Route path='/book-appointment' element={<BookPage />}/> */}
         </Routes>
     </Router>
---
+++ src/View/pages/gallery/Gallery.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './gallery.scss';
+import nopost from '../../assets/images/nopost.png'
+import Navbar from '../../components/navigation/Navbar'
+import Footer from '../../components/footer/Footer'
+
+function Gallery() {
+  return (
+    <div className='gallery-container'>
+        <Navbar />
+
+        <div className="gallery-banner">
+            <div className='gallery-darkOverlay'></div>
+            <div className="gallery-header">
+                    <p>Gallery</p>
+            </div>
+        </div>
+
+        <div className="gallery-nothing">
+            <div className="gallery-wrapper">
+              <div className='gallery-image'>
+                <img src={nopost} alt='no post'/>
+              </div>
+              <div className='gallery-text'>
+                <p>No Media Files</p>
+                <p>There are no gallery posts available at the moment.</p>
+              </div>
+            </div>
+        </div>
+
+        <Footer />
+        
+    </div>
+  )
+}
+
+export default Gallery
