cdd3b49099d00fe447c3a5412fad78eea8f9872f
Obinna Okere <okereobinna10@gmail.com>
2023-10-03 14:20:49 +0100

    Blog Page Added bot for mobile and desktop view

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './View/pages/home/Home'
-import AboutUs from './View/pages/about/AboutUs'
-// import Blog from './View/pages/blog/Blog'
+import Home from './View/pages/home/Home';
+import AboutUs from './View/pages/about/AboutUs';
+import Blog from './View/pages/blog/Blog';
 // import Gallery from './View/pages/gallery/Gallery'
 // import Contact from './View/pages/contact/Contact'
 // import BookPage from './View/pages/bookPage/BookPage'
@@ -13,8 +13,8 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />}/>
           <Route path='/about-us' element={<AboutUs />}/>
-          {/* <Route path='/blog' element={<Blog />}/>
-          <Route path='/gallery' element={<Gallery />}/>
+          <Route path='/blog' element={<Blog />}/>
+          {/* <Route path='/gallery' element={<Gallery />}/>
           <Route path='/contact-us' element={<Contact />}/>
           <Route path='/book-appointment' element={<BookPage />}/> */}
         </Routes>
---
+++ src/View/pages/blog/Blog.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './blog.scss';
+import nopost from '../../assets/images/nopost.png'
+import Navbar from '../../components/navigation/Navbar';
+import Footer from '../../components/footer/Footer';
+
+function Blog() {
+  return (
+    <div className='blog-container'>
+        <Navbar />
+
+        <div className="blog-banner">
+            <div className='blog-darkOverlay'></div>
+            <div className="blog-header">
+                    <p>Blog Post</p>
+            </div>
+        </div>
+
+        <div className="blog-nothing">
+            <div className="blog-wrapper">
+              <div className='blog-image'>
+                <img src={nopost} alt='no post'/>
+              </div>
+              <div className='blog-text'>
+                <p>No Posts Yet</p>
+                <p>There are no blog posts available at the moment.</p>
+              </div>
+            </div>
+        </div>
+
+        <Footer />
+        
+    </div>
+  )
+}
+
+export default Blog
