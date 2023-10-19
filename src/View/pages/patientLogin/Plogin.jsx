79350a8a5dbe2400eb4936e813cb21ffb29a0135
Obinna Okere <okereobinna10@gmail.com>
2023-10-19 15:25:14 +0100

    reset password page.

--- src/View/pages/patientLogin/Plogin.jsx
+++ src/View/pages/patientLogin/Plogin.jsx
@@ -9,7 +9,8 @@ function Plogin() {
     //     password: ""
     // });
     const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('')
+    const [password, setPassword] = useState('');
+    const[activeStep, setActiveStep] = useState(1)
 
     // const handleInputChange = e => {
     //     setFormData({...formData, [e.target.value]:e.target.value});
@@ -25,34 +26,67 @@ function Plogin() {
             <div className="plogin-wrapper-right">
                 
                 <div className="form-holder">
-                    <div className="headerText">
-                        <h3>Hello! <span>Welcome</span> Back</h3>
+                    <div className={`phase1 ${activeStep === 1 ? "active" : "inactive" }`}>
+                        <div className="headerText">
+                            <h3>Hello! <span>Welcome</span> Back</h3>
+                        </div>
+                        <form action="">
+                            <section>
+                                <input 
+                                type="text" 
+                                placeholder='Email'
+                                name='email'
+                                value={email}
+                                // onChange={handleInputChange}
+                                onChange = {(e)=>setEmail(e.target.value)}
+                                className="formInput"
+                                />
+                                <input 
+                                type="password" 
+                                placeholder='Password'
+                                name='password'
+                                value={password}
+                                // onChange={handleInputChange}
+                                onChange = {(e)=>setPassword(e.target.value)}
+                                className="formInput"
+                                />
+                            </section>
+                            
+                            <button type='submit'>Login</button>
+
+                            <div className="sub-info" onClick={(e)=>setActiveStep(2)}>
+                                Forgot Password?
+                            </div>
+                            
+                        </form>
                     </div>
-                    <form action="">
-                        <section>
-                            <input 
-                            type="text" 
-                            placeholder='Email'
-                            name='email'
-                            value={email}
-                            // onChange={handleInputChange}
-                            onChange = {(e)=>setEmail(e.target.value)}
-                            className="formInput"
-                            />
-                            <input 
-                            type="password" 
-                            placeholder='Password'
-                            name='password'
-                            value={password}
-                            // onChange={handleInputChange}
-                            onChange = {(e)=>setPassword(e.target.value)}
-                            className="formInput"
-                            />
-                        </section>
+                    <div className={`phase2 ${activeStep === 2 ? "active" : "inactive" } `}>
+                        <div className="headerText">
+                            <h3>Reset <span>Password</span></h3>
+                            <p>Enter the email address associated with your account and we'll send you a reset password.</p>
+                        </div>
+                        <form action="">
+                            <section>
+                                <input 
+                                type="text" 
+                                placeholder='Email'
+                                name='email'
+                                value={email}
+                                // onChange={handleInputChange}
+                                onChange = {(e)=>setEmail(e.target.value)}
+                                className="formInput"
+                                />
+                            </section>
+                            
+                            <button type='submit'>Send</button>
+
+                            <div className="sub-info" onClick={(e)=>setActiveStep(1)}>
+                                Have an account? <span>Sign Up</span>
+                            </div>
+                            
+                        </form>
                         
-                        <button type='submit'>Login</button>
-                          
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
