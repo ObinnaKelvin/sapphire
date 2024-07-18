221fbf3be56f8a00b312e953257c0b251b385df4
Obinna Okere <okereobinna10@gmail.com>
2024-07-18 19:59:59 +0100

    React-snap installed

--- src/index.js
+++ src/index.js
@@ -1,18 +1,39 @@
 import React from 'react';
+import { hydrate, render } from "react-dom";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AuthContextProvider } from './View/context/AuthContext';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
-  </React.StrictMode>
-);
+//Setup 1
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <AuthContextProvider>
+//       <App />
+//     </AuthContextProvider>
+//   </React.StrictMode>
+// );
+
+//Setup 2
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(
+    <React.StrictMode>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </React.StrictMode>, rootElement);
+} else {
+  render(
+    <React.StrictMode>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </React.StrictMode>, rootElement);
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
