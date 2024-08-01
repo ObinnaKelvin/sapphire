f25224b409d284ecbddda3524bd9257b3c70427b
Obinna Okere <okereobinna10@gmail.com>
2024-08-01 03:36:08 +0100

    Animation added to service section of homepage

--- src/View/components/services/Services.jsx
+++ src/View/components/services/Services.jsx
@@ -8,74 +8,125 @@ import thyroid from '../../assets/images/thyroid.png';
 import breast from '../../assets/images/breasts.png';
 import endoscope from '../../assets/images/endoscope.png';
 import oncology from '../../assets/images/oncology.png';
+import { animate, delay, motion, transform, useInView } from "framer-motion";
+
+
+const spikesVariants = {
+    // initial: {
+    //     x: -100,
+    //     opacity: 0
+    // },
+    initial: {
+        x: 0,
+        opacity: 0,
+        rotate: 0,
+    },
+    animate: {
+        x: 0,
+        opacity: 1,
+        rotate: 35.277,
+        transition: {
+            duration: 1.5
+        }
+    }
+}
+
+const servicesVariants = {
+    initial: {
+        x: -100,
+        opacity: 0
+    },
+    animate: {
+        x: 0,
+        opacity: 1,
+        transition: {
+            staggerChildren: 0.5
+        }
+    }
+}
+
+const servicesItemVariants = {
+    initial: {
+        x: -100,
+        opacity: 0
+    },
+    
+    animate: {
+        x: 0,
+        opacity: 1,
+        transition: {
+            duration: 1.5
+        }
+    }
+}
 
 const Services = () => {
   return (
-    <div className='services-container'>
+    <motion.div className='services-container'>
         <div className="services-wrapper">
             <div className="services-header">
-                <div className="spikes">
+                <motion.div className="spikes" variants={spikesVariants} initial='initial' whileInView="animate">
                     <div className="spike spikeOne"></div>
                     <div className="spike spikeTwo"></div>
                     <div className="spike spikeThree"></div>
-                </div>
+                </motion.div>
                 <div className="services-header-text">
                     Our Specialist Services
                 </div>
             </div>
-            <div className="services-items">
-                <div className="services-item">
+            <motion.div className="services-items" initial='initial' variants={servicesVariants} whileInView='animate'>
+                <motion.div className="services-item"  initial='initial' variants={servicesItemVariants} whileInView='animate'>
                     <div className="img-holder">
                         <img src={scapel} alt='scapel for surgery'/>
                     </div>
                     <p>Minimal Access Surgery</p>
-                </div>
-                <div className="services-item">
+                </motion.div>
+                <motion.div className="services-item" initial='initial' variants={servicesItemVariants} whileInView='animate'>
                     <div className="img-holder">
                         <img src={kidney} alt='Urology surgery'/>
                     </div>
                     <p>Urological Surgery</p>
-                </div>
-                <div className="services-item">
+                </motion.div>
+                <motion.div className="services-item" initial='initial' variants={servicesItemVariants} whileInView='animate'>
                     <div className="img-holder">
                         <img src={bariatric} alt='Bariatric surgery'/>
                     </div>
                     <p>Bariatric Surgery</p>
-                </div>
-                <div className="services-item">
+                </motion.div>
+                <motion.div className="services-item" initial='initial' variants={servicesItemVariants} whileInView='animate'>
                     <div className="img-holder">
-                        <img src={intestine} alt='Gastrointestinal surgery'/>
+                    <img src={intestine} alt='Gastrointestinal surgery'/>
                     </div>
                     <p>Gastrointestinal Surgery</p>
-                </div>
-                <div className="services-item">
+                </motion.div>
+                <motion.div className="services-item" initial='initial' variants={servicesItemVariants} whileInView='animate'>
                     <div className="img-holder">
                         <img src={thyroid} alt='Thyroid surgery'/>
                     </div>
                     <p>Thyroid Surgery</p>
-                </div>
-                <div className="services-item">
+                </motion.div>
+                <motion.div className="services-item" initial='initial' variants={servicesItemVariants} whileInView='animate'>
                     <div className="img-holder">
                         <img src={breast} alt='Breast surgery'/>
                     </div>
                     <p>Breast Surgery</p>
-                </div>
-                <div className="services-item">
+                </motion.div>
+                <motion.div className="services-item" initial='initial' variants={servicesItemVariants} whileInView='animate'>
                     <div className="img-holder">
                         <img src={endoscope} alt='Diagnostic and Therapeutic Endoscopy'/>
                     </div>
                     <p>Diagnostic and Therapeutic Endoscopy</p>
-                </div>
-                <div className="services-item">
+                </motion.div>
+                <motion.div className="services-item" initial='initial' variants={servicesItemVariants} whileInView='animate'>
                     <div className="img-holder">
                         <img src={oncology} alt='Surgical Oncology'/>
                     </div>
                     <p>Surgical Oncology</p>
-                </div>
-            </div>
+                </motion.div>
+            </motion.div>
         </div>
 
-    </div>
+    </motion.div>
   )
 }
 
